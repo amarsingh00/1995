8f31cbafe9877477a536cd3ae6480c0c6a249938
amarsingh00 <amar.sachdeva@gmail.com>
2021-02-11 08:10:14 +1000

    Passing value to a function

--- functions_sample2.js
+++ functions_sample2.js
@@ -6,3 +6,11 @@ function greet(print_name){
 
 let name0 = 'Amar'
 greet(name0)
+
+//when we pass value to the function, we call it argument.
+
+function greet2(print_name, print_day){
+    console.log('Hello  ', print_name, 'Day is  ', print_day)
+}
+
+greet2(name0)
