e71ce18f512f928d689134f03498e317889e94e6
Amar Singh <amar.sachdeva@gmail.com>
2021-02-13 11:27:09 +1000

    function hositing vs function expression

---
+++ functions_sample6.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// function hoisting
+function Greet1(name0){
+    console.log('Hello  '+name0)
+}
+
+// Greet2(nameX)
+
+// function expression
+// in this function must be defined before it can be called.
+// ReferenceError: Cannot access 'Greet2' before initialization
+
+let Greet2 = function(name0){
+    console.log('Hello '+ name0)
+}
+
+let nameX = 'Amar'
+
+Greet1(nameX)
+Greet2(nameX)
